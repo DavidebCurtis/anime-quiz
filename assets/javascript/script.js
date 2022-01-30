182edcfd54eb126407bf6cbc64b78b055c93680e
david curtis <heythereimdavidcurtis@gmail.com>
2022-01-30 14:50:44 -0500

    pseudo functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,8 +7,14 @@ var timerEL = document.getElementById("timer");
 var timerContainerEL = document.getElementById("timer-container");
 var yourScore = document.getElementById("your-score");
 var highScoresPrompt = document.getElementById("highscores-prompt-container");
+var submitHS = document.getElementById("submit-hs");
+var userInput = document.getElementById("userinput");
+var highScoresEL = document.getElementById("highscores-container");
+var highScoresLI = document.getElementById("highscore-list");
 
 let shuffledQuestions, currentQuestionIndex;
+
+var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 let count = 40;
 // Timer
@@ -21,17 +27,51 @@ var timer = function () {
       questionsContainer.style.display = "none";
       timerContainerEL.style.display = "none";
       highScoresPrompt.style.display = "flex";
+      saveHighScore();
       return;
     }
     if (shuffledQuestions.length < currentQuestionIndex + 1) {
-      yourScore.innerHTML = count + 1;
+      yourScore.innerHTML = count;
       clearInterval(counter);
       setTimeout(displayHighScorePrompt, 800);
+      saveHighScore();
     }
 
     timerEL.innerHTML = count;
   }, 1000);
 };
+
+// save highscores to local storage
+function saveHighScore() {
+  submitHS.addEventListener("click", function () {
+    const score = {
+      score: count,
+      name: userInput.value,
+    };
+    highScores.push(score);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    highScoresPrompt.style.display = "none";
+    highScoresEL.style.display = "flex";
+    // displayHS();
+    // console.log(highScores);
+    for (let index = 0; index < highScores.length; index++) {
+      const makeLI = document.createElement("li");
+      makeLI.innerText = highScores[index].score + highScores[index].name;
+      console.log(highScores[index]);
+      highScoresLI.appendChild(makeLI);
+    }
+  });
+}
+
+// insert highscores from local storage in to element
+
+// function displayHS() {
+//   for (let index = 0; index < highScores.length; index++) {
+//     const makeLI = document.createElement("li");
+//     makeLI.innerText = score.name;
+//     highScoresLI.appendChild(makeLI);
+//   }
+// }
 
 // start button begins quiz and starts timer
 startBtn.addEventListener("click", function () {
@@ -39,7 +79,7 @@ startBtn.addEventListener("click", function () {
   timer();
 });
 
-// waits and then shows high score prompt
+// waits and then shows high score prompt if completed in time. this stops a div from showing on screen for a split second
 function displayHighScorePrompt() {
   questionsContainer.style.display = "none";
   timerContainerEL.style.display = "none";
@@ -58,18 +98,18 @@ var startQuiz = function () {
 // picks next question randomly and displays it in the element
 var displayQuestion = function () {
   reset();
-  //   if (shuffledQuestions.length < currentQuestionIndex + 1) {
-  //     questionsContainer.style.display = "none";
-  //     timerContainerEL.style.display = "none";
-  //     highScoresPrompt.style.display = "flex";
-  //     return;
-  //   }
   showQuestion(shuffledQuestions[currentQuestionIndex]);
   //   showQuestion(questions[0]);
 };
 
 // displays questions
 function showQuestion(question) {
+  if (shuffledQuestions.length < currentQuestionIndex + 1) {
+    questionsContainer.style.display = "none";
+    timerContainerEL.style.display = "none";
+    highScoresPrompt.style.display = "flex";
+    return;
+  }
   questionEl.innerText = question.question;
   // loops through answers and creates button elements for each
   question.answers.forEach((answer) => {
